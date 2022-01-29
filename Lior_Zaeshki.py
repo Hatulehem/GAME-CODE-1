a8c7658c4d4f4920cf306cfa01184fca8b3af694
Hatulehem <benaya7890@gmail.com>
2022-01-29 21:10:11 +0200

    updated

--- Lior_Zaeshki.py
+++ Lior_Zaeshki.py
@@ -1,56 +1,76 @@
-from pickle import TRUE
 import pygame
 
-#screen 
+#screen size
 WINDOW_W = 900
 WINDOW_H = 500
+WINDOW_SIZE = (WINDOW_W,WINDOW_H)
 
-#init screen
 pygame.init()
-size = (WINDOW_W,WINDOW_H)
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Hypnosis Arc: The Neko Battle")
 
-#משתנים
+#images/variables
+bk_img = pygame.image.load("bk_neko arc.jpg")
+nekoarc_img = pygame.image.load("neko player 1.png")
+nekoarc_img = pygame.transform.scale(nekoarc_img,(115,123))
+pizza_img = pygame.image.load("pizza.png")
+pizza_img = pygame.transform.scale(pizza_img,(110,110))
+
 clock = pygame.time.Clock()
-x_player = 450
-y_player = 430
-vel = 2
+
+pizza_img_x = 10
+pizza_img_y = WINDOW_H /2
+nekoarc_img_x = WINDOW_W /2
+nekoarc_img_y = WINDOW_H - 123
+
+pizza_img_x_step = 10
+neko_x_step = 10
+pizza_list = []
+
+#function
+def print_pizza():
+    for i in range (len(pizza_list)):
+        p = pizza_list[i]
+    screen.blit(pizza_img, (p[0],p[1]))
+    pizza_list[i] = [p[0],p[1] - 60]
+
+    if len(pizza_list) > 0 and pizza_list[0][1] < 0:
+        pizza_list.remove(pizza_list[0])
+
+
+
+
+
+
+
+
+
 
 #loop
-RUN =   True
-while RUN:
+play = True
+
+while play:
+    screen.blit(bk_img , (0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            RUN = False
+            play = False
+        elif event.type == pygame.KYDOWN:
+            if event.key == pygame.K_LEFT:
+                nekoarc_img_x -= neko_x_step
+            if event.key == pygame.K_RIGHT:
+                nekoarc_img_x += neko_x_step
+            if event.key == pygame.K_SPACE:
+                pizza_list.append([nekoarc_img_x, 115/2 , nekoarc_img_y])
+    
+    screen.blit(nekoarc_img, nekoarc_img_x,nekoarc_img_y)
+    print_pizza()
 
-#Fill Screen
-    #background
-    img = pygame.image.load("neko arc.jpg")
-    screen.blit(img , (0,0))
-    #player
-    bg_player_color = (115,152,255)
-    player_img = pygame.image.load("neko ar.png").convert()
-    player_img.set_colorkey(bg_player_color)
-    screen.blit(player_img, [x_player,y_player])
 
-#keys 
-    keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        x_player -= vel
-    if keys[pygame.K_RIGHT]:
-        x_player += vel
-    if keys[pygame.K_UP]:
-        y_player -= vel
-    if keys [pygame.K_DOWN]:
-        y_player += vel
-
-    if x_player <= 0:
-        x_player = 70
-    if x_player >= 900:
-        x_player = 830
 
     pygame.display.flip()
+
+    clock.tick(10)
 
 pygame.quit()
