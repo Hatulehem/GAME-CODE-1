e2c7914f4f73242fa9f715ef2b5cc5f29f87f67f
Hatulehem <benaya7890@gmail.com>
2022-02-02 01:35:30 +0200

    updated!

--- Lior_Zaeshki.py
+++ Lior_Zaeshki.py
@@ -11,36 +11,47 @@ pygame.display.set_caption("Hypnosis Arc: The Neko Battle")
 
 #images/variables
 bk_img = pygame.image.load("bk_neko arc.jpg")
+
 nekoarc_img = pygame.image.load("neko player 1.png")
 nekoarc_img = pygame.transform.scale(nekoarc_img,(115,123))
+
+dog_img = pygame.image.load("dog1.png")
+red = (242,6,0)
+dog_img.set_colorkey(red)
+
+toy_chika = pygame.image.load("toy chica lol.png")
+toy_chika = pygame.transform.scale(toy_chika, (80,80))
+green = (7,248,19)
+toy_chika.set_colorkey(green)
+
+neko_music = "neko music.08"
+pygame.mixer.init()
+pygame.mixer.music.load(neko_music)
+pygame.mixer.music.play()
 
 
 clock = pygame.time.Clock()
 
 neko_x = WINDOW_W /2
 neko_y = WINDOW_H - 123
-
 vel_neko = 8
-# pizza_img_x_step = 10
-# neko_x_step = 10
-# pizza_list = []
+
+dog_list = []
+
+toy_x = 450
+toy_y = 100
+toy_x_step = 20
+
 
 #function
-# def print_pizza():
-#     for i in range (len(pizza_list)):
-#         p = pizza_list[i]
-#         screen.blit(pizza_img, (p[0],p[1]))
-#         pizza_list[i] = [p[0],p[1] - 60]
+def print_dog():
+  for i in range(len(dog_list)):
+    l = dog_list[i]
+    screen.blit(dog_img,(l[0],l[1]))
+    dog_list[i] = [l[0],l[1]-27]
 
-#     if len(pizza_list) > 0 and pizza_list[0][1] < 0:
-#         pizza_list.remove(pizza_list[0])
-
-
-
-
-
-
-
+  if len(dog_list) > 0 and dog_list[0][1] < 0:
+    dog_list.remove(dog_list[0])
 
 
 
@@ -48,12 +59,23 @@ vel_neko = 8
 play = True
 
 while play:
+    #images that will appeer
     screen.blit(bk_img , (0,0))
     screen.blit(nekoarc_img, (neko_x - 115/2,neko_y))
+    screen.blit(toy_chika , (toy_x-50,toy_y))
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dog_list.append([neko_x,neko_y])
+
+                gurnya = "gurnya.mp3"
+                pygame.mixer.init()
+                pygame.mixer.music.load(gurnya)
+                pygame.mixer.music.play()
 
     #keys
     keys = pygame.key.get_pressed()
@@ -68,7 +90,16 @@ while play:
         neko_y -= vel_neko
 
 
+    toy_x +=toy_x_step
+    if toy_x > WINDOW_W:
+        toy_x_step = -10
+    if toy_x <0 :
+        toy_x_step = 10
+    #others
     pygame.display.update()
+
+    print_dog()
+
     pygame.display.flip()
 
     clock.tick(20)
